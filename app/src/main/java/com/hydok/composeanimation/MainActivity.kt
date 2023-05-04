20f57bd1740d409ee05b1a95a255e910aee594f3
hydok <hydok@naver.com>
2023-05-04 11:13:52 +0900

    main

--- app/src/main/java/com/hydok/composeanimation/MainActivity.kt
+++ app/src/main/java/com/hydok/composeanimation/MainActivity.kt
@@ -3,13 +3,21 @@ package com.hydok.composeanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.hydok.composeanimation.snow.SnowScreen
 import com.hydok.composeanimation.ui.theme.ComposeAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +25,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAnimationTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = "main") {
+                        composable(route = "main") {
+                            Column(
+                                modifier = Modifier.fillMaxSize().padding(10.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                MyButton("SNOW") {
+                                    navController.navigate("snow")
+                                }
+                            }
+                        }
+                        composable(route = "snow") {
+                            SnowScreen()
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeAnimationTheme {
-        Greeting("Android")
+fun MyButton(str: String, click: () -> Unit) {
+    Button(
+        onClick = click,
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black),
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier.fillMaxWidth(0.8f)
+    ) {
+        Text(text = str, color = Color.White)
     }
 }
